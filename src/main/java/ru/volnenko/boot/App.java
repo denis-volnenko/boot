80a3b9b504d30cfa93731ed688bdcd4003a6b653
Denis Volnenko <denis@volnenko.ru>
2021-03-17 21:33:32 +0400

    Added soap server.

--- src/main/java/ru/volnenko/boot/App.java
+++ src/main/java/ru/volnenko/boot/App.java
@@ -1,13 +1,27 @@
 package ru.volnenko.boot;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+import java.util.UUID;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@WebService
+public class App {
+
+    @WebMethod
+    public String uuid() {
+        return UUID.randomUUID().toString();
     }
+
+    public static void main( String[] args ) {
+        final String wsdl = "http://0.0.0.0:8080/App?wsdl";
+        System.out.println(wsdl);
+        Endpoint.publish(wsdl, new App());
+    }
+
 }
